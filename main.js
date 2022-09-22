64ba8306c6286caea837feab8227bf225b085cd1
Piyush Kumar <piyu8h@gmail.com>
2022-09-22 15:40:13 +0530

    beautify

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function onSubmit(e) {
   const inputField = document.querySelectorAll('.inputField');
   e.preventDefault();
   let check = false;
-  for(let input of inputField){
+  for (let input of inputField) {
     if (input.value === '') {
       check = true;
     }
@@ -30,34 +30,33 @@ function onSubmit(e) {
     msg.classList.add('error');
     msg.innerHTML = 'Please enter all fields';
     setTimeout(() => msg.remove(), 3000);
-  }else {
-      inputField.forEach(function(input){
+  } else {
+    inputField.forEach(function (input) {
       input.style.display = 'none';
-      }
-    )
-      btn.style.display = 'none';
-    
-    for(let i = 0; i < ansList.length; i++){
-      const correctAns = inputField[2*i].value ;
-      const incorrectAns = inputField[2*i+1].value;
+    })
+    btn.style.display = 'none';
 
-      totalCorrect += 4*parseInt(correctAns);
+    for (let i = 0; i < ansList.length; i++) {
+      const correctAns = inputField[2 * i].value;
+      const incorrectAns = inputField[2 * i + 1].value;
+
+      totalCorrect += 4 * parseInt(correctAns);
       totalIncorrect -= parseInt(incorrectAns);
 
       const str = `${4*correctAns} - ${incorrectAns} = ${4*correctAns - incorrectAns}`;
-      
+
       ansList[i].classList.add('makeInlineLeft');
       marksList[i].classList.add('makeInlineRight');
-      
+
       marksList[i].textContent = str;
     }
     if (totalCorrect > totalMarks) {
       msg.innerHTML = `<h2>Enter Marks Correctly (${totalCorrect} / ${totalMarks})</h2>`;
       msg.classList.add('error');
-    }else{
+    } else {
       msg.innerHTML = `<h2>You have scored ${totalCorrect} out of ${totalMarks}</h2>`;
       msg.classList.add('success');
     }
-    
+
   }
 }
